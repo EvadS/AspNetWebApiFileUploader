f6ba152f264f082058c69c500ded35ff1154d6f9
Евгений Скиба < @MOBIUS-7>
2016-05-25 12:39:52 +0300

    загрузка файлов. FileUploadController/ UploadFile

---
+++ FileUploader/Controllers/ValuesController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace FileUploader.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET: api/Values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+      
+
+        // PUT: api/Values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Values/5
+        public void Delete(int id)
+        {
+        }
+
+        [HttpPost]
+        public async Task<IHttpActionResult> Upload()
+        {
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                return BadRequest();
+            }
+
+            var provider = new MultipartMemoryStreamProvider();
+            // путь к папке на сервере
+            string root = System.Web.HttpContext.Current.Server.MapPath("~/Files/");
+            await Request.Content.ReadAsMultipartAsync(provider);
+
+            foreach (var file in provider.Contents)
+            {
+                var filename = file.Headers.ContentDisposition.FileName.Trim('\"');
+                byte[] fileArray = await file.ReadAsByteArrayAsync();
+
+                using (System.IO.FileStream fs = new System.IO.FileStream(root + filename, System.IO.FileMode.Create))
+                {
+                    await fs.WriteAsync(fileArray, 0, fileArray.Length);
+                }
+            }
+            return Ok("файлы загружены");
+        }
+    }
+}
