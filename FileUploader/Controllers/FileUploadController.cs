12eb54f41d2697d6c5948d17cee091d042529d42
Евгений Скиба < @MOBIUS-7>
2016-07-06 12:47:51 +0300

    working at progress bar - console version

--- FileUploader/Controllers/FileUploadController.cs
+++ FileUploader/Controllers/FileUploadController.cs
@@ -20,21 +20,37 @@ namespace FileUploader.Controllers
         [HttpPost]
         public void UploadFile()
         {
+            // todo: for test
+            if (HttpContext.Current.Request.Form.AllKeys.Any())
+            {
+                string currentUserName = HttpContext.Current.Request.Form["userName"];
+                string s = currentUserName;
+            }
+
             if (HttpContext.Current.Request.Files.AllKeys.Any())
             {
+
                 // Get the uploaded image from the Files collection
                 var httpPostedFile = HttpContext.Current.Request.Files["UploadedImage"];
+                var userName = HttpContext.Current.Request.Files["userName"];
 
                 if (httpPostedFile != null)
                 {
                     // Validate the uploaded image(optional)
+                    try {
+                        // Get the complete file path
+                        var path = HttpContext.Current.Server.MapPath("~/UploadedFiles");
+                        var fileSavePath = Path.Combine(HttpContext.Current.Server.MapPath("~/UploadedFiles"), httpPostedFile.FileName);
 
-                    // Get the complete file path
-                    var fileSavePath = Path.Combine(HttpContext.Current.Server.MapPath("~/UploadedFiles"), httpPostedFile.FileName);
-
-                    // Save the uploaded file to "UploadedFiles" folder
-                    httpPostedFile.SaveAs(fileSavePath);
-                }
+                        // Save the uploaded file to "UploadedFiles" folder
+                        httpPostedFile.SaveAs(Path.Combine(path, httpPostedFile.FileName));
+                        File.SetAttributes(fileSavePath, FileAttributes.Normal);
+                    }
+                    catch(Exception ex )
+                    {
+                        int a = 10;
+                    }
+                    }
             }
         }
     }
